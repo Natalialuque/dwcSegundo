d6b14573c4621150f577d6478154c912b811c736
natalia <tuncabluq878@g.educaand.es>
2026-09-25 10:30:20 +0200

    secon day

--- Pruebas/prueba1.js
+++ Pruebas/prueba1.js
@@ -1,34 +1,75 @@
 //llamada de main principal
-const main = document.getElementById("main1");
+    // const main = document.getElementById("main1");
 
 //creacion de boton con modulo
-var newBoton = document.createElement("button");
-newBoton.innerHTML="Hola";
-newBoton.style.backgroundColor="red";
+    // var newBoton = document.createElement("button");
+    // newBoton.innerHTML="Hola";
+    // newBoton.style.backgroundColor="red";
 
 //añadirlo en el main 
-main.appendChild(newBoton);
+    // main.appendChild(newBoton);
 
 //para añadir eventos 
-newBoton.addEventListener("click",function(){
-    //alert("hemos pulsado el boton");
+    // newBoton.addEventListener("click",function(){
+    //     //alert("hemos pulsado el boton");
 
-    if(newBoton.style.backgroundColor==="red"){
-        newBoton.style.backgroundColor="white";
-    }else{
-        newBoton.style.backgroundColor="red";
-    }
+    //     if(newBoton.style.backgroundColor==="red"){
+    //         newBoton.style.backgroundColor="white";
+    //     }else{
+    //         newBoton.style.backgroundColor="red";
+    //     }
 
-});
+    // });
 
 //SEGUNDA PRUEBA --> mostrar por consola el nombre de una variable
-let nombre = "pepe";
-console.log("nombre de "+nombre);
+    // let nombre = "pepe";
+    // console.log("nombre de "+nombre);
+
+    // //
+    // nombre = 5;
+    // console.log("nombre de "+nombre);
 
 //
-nombre = 5;
-console.log("nombre de "+nombre);
+// nombre = true;
+// console.log(nombre);
 
-//
-nombre = true;
-console.log(nombre);
+/**
+ * Declaramos las variables
+ * las variables var son globales
+ * let es local al bloque en el que se crea
+ */
+        // let a = 5;
+        // var b = 10;
+        // var c = 20;
+
+        // console.log("Valor de a: " + a + " valor de b: " + b );
+
+        // if (a < 10) {
+        //     showA();
+        // }
+
+
+//funciones
+        // function showA(){
+        //     console.log("El valor de a es menor de 10 es: " + a)
+            
+        // }
+
+        // console.dir("hola");
+
+/**funcion concatenacion de sumas  */
+// var d=5;
+// document.getElementById("numeroCarrito").addEventListener("input",function(){
+//     a+=parseInt(this.value);
+//     console.log(d);
+// })
+
+/**Array de literales */
+// var alumno = ["pepe",true,5.67,null,{name:"ana",curso:"2daw"},[7,8,9.5]];
+// console.log(alumno);
+
+
+/**
+ * le pongo value en el js al campo texto 
+ */
+document.getElementById("iti1").value="Hola";
